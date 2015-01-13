75b5f74c36a99c1674bf50f13644563cd06dff78
claptonvillanueva@gmail.com <claptonvillanueva@gmail.com@ad3ff22a-c983-2158-72a6-b75097ad4e9c>
2015-01-13 16:02:45 +0000

    + created basic Template

---
+++ trunk/Bubble Pop/Assets/Scripts/GUI/MainMenuController.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MainMenuController : MonoBehaviour {
+
+	public static MainMenuController instance;
+	
+	void Awake () {
+		instance = this;
+	}
+
+	public void LoadGameScene() {
+		Debug.Log("Load Game Scene");
+	}
+	
+	public void OpenSettings() {
+		Debug.Log("Settings");
+	}
+}
---
+++ trunk/Bubble Pop/Assets/Scripts/GameController/GameController.cs
@@ -0,0 +1,117 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public enum GameState {
+	main = 0,
+	playing,
+	paused,
+	gameOver,
+	playAgain,
+}
+
+public class GameController : MonoBehaviour {
+
+	public static GameController instance { get; private set; }
+
+	public GameState gameState = GameState.main;
+	public int highScore;
+	public int playerScore;
+	public bool isPaused = false;
+
+	public int HighScore {
+		get { 
+			if(PlayerPrefs.HasKey("HighScore")) {
+				highScore = PlayerPrefs.GetInt("HighScore");
+				return  highScore;
+			} else {
+				highScore = 0;
+				PlayerPrefs.SetInt("HighScore", 0);
+				PlayerPrefs.Save();
+				return 0;
+			}
+		}
+		set {
+			highScore = value;
+			PlayerPrefs.SetInt("HighScore", value);
+			PlayerPrefs.Save();
+		}
+	}
+
+	void Awake() {
+		if(instance != null && instance != this) {
+			Destroy(this.gameObject);
+			return;
+		}
+
+		instance = this;
+		DontDestroyOnLoad(this.gameObject);
+	}
+
+	void Start() {
+		Init ();
+	}
+
+	private void Init() {
+		highScore = HighScore;
+	}
+
+	public void ChangeState(GameState p_newState) {
+		gameState = p_newState;
+		ProcessGameState();
+	}
+
+	private void ProcessGameState() {
+		switch(gameState) {
+		case GameState.main:
+			LoadMainScene();
+			break;
+		case GameState.playing:
+			Resume();
+			break;
+		case GameState.paused:
+			Pause();
+			break;
+		case GameState.gameOver:
+			GameOver();
+			break;
+		case GameState.playAgain:
+			PlayAgain();
+			break;
+		}
+	}
+
+	private void LoadMainScene() {
+		Application.LoadLevel("MainScene");
+	}
+
+	private void Pause() {
+		isPaused = true;
+		Time.timeScale = 0;
+	}
+
+	private void Resume() {
+		isPaused = false;
+		Time.timeScale = 1;
+	}
+
+	private void GameOver() {
+		Debug.Log("GAME OVER!!!");
+		HighScore = (playerScore > HighScore) ? playerScore : HighScore;
+		// TODO GameHUD - show Score and HighScore
+	}
+
+	public IEnumerator LoadLevel() {
+		Application.LoadLevel("GameScene");
+		yield return null;
+		PlayAgain();
+	}
+
+	private void PlayAgain() {
+		Debug.Log("PLAY AGAIN");
+		// TODO Clean all object instance including player instance
+		// TODO Restart GameHud
+		// TODO Restart Level
+		// TODO Instantiate player instance
+		// TODO ChangeState(GameState.playing)
+	}
+}
