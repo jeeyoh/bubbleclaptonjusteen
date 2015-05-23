e53c999993a4f16b68f690b583306c1a046529f5
ketsana <clapton_villanueva@yahoo.com>
2015-05-23 22:29:25 +0800

    Added No Black Bubbles reward for each game mode

--- Bubble Pop/Assets/Scripts/PlayerPrefsManager.cs
+++ Bubble Pop/Assets/Scripts/PlayerPrefsManager.cs
@@ -11,9 +11,18 @@ public static class PlayerPrefsManager {
 	public const string ENDLESS_MODE_5_BEST_TIME = "EndlessMode5BestTime";
 	public const string ENDLESS_MODE_25_BEST_TIME = "EndlessMode25BestTime";
 	public const string ENDLESS_MODE_50_BEST_TIME = "EndlessMode50BestTime";
-	public const string NO_BLACK_BUBBLES_COUNT = "NoBlackBubblesCount";
-	public const string TIME_MODE_SUCCEEDING_WINS = "TimeModeSucceedingWins";
-	public const string ENDLESS_MODE_SUCCEEDING_WINS = "EndlessModeSucceedingWins";
+	public const string NO_BLACK_BUBBLES_COUNT_TIME_MODE_50 = "NoBlackBubblesCountTimeMode50";
+	public const string NO_BLACK_BUBBLES_COUNT_TIME_MODE_100 = "NoBlackBubblesCountTimeMode100";
+	public const string NO_BLACK_BUBBLES_COUNT_TIME_MODE_150 = "NoBlackBubblesCountTimeMode150";
+	public const string NO_BLACK_BUBBLES_COUNT_ENDLESS_MODE_5 = "NoBlackBubblesCountEndlessMode5";
+	public const string NO_BLACK_BUBBLES_COUNT_ENDLESS_MODE_25 = "NoBlackBubblesCountEndlessMode25";
+	public const string NO_BLACK_BUBBLES_COUNT_ENDLESS_MODE_50 = "NoBlackBubblesCountEndlessMode50";
+	public const string TIME_MODE_50_SUCCEEDING_WINS = "TimeMode50SucceedingWins";
+	public const string TIME_MODE_100_SUCCEEDING_WINS = "TimeMode100SucceedingWins";
+	public const string TIME_MODE_150_SUCCEEDING_WINS = "TimeMode150SucceedingWins";
+	public const string ENDLESS_MODE_5_SUCCEEDING_WINS = "EndlessMode5SucceedingWins";
+	public const string ENDLESS_MODE_25_SUCCEEDING_WINS = "EndlessMode25SucceedingWins";
+	public const string ENDLESS_MODE_50_SUCCEEDING_WINS = "EndlessMode50SucceedingWins";
 
 	public static bool GetBool (string p_key, bool p_defaultValue) {
 		if(PlayerPrefs.HasKey(p_key)) {
