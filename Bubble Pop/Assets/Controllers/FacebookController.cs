05030f39ab40a93f84a1a6e03b7e907e0171d0ab
Justeen Mendoza <justeenm@bubbleguminteractive.com>
2015-05-25 08:57:52 +0800

    + new facebook plugin

---
+++ Bubble Pop/Assets/Controllers/FacebookController.cs
@@ -0,0 +1,152 @@
+﻿using UnityEngine;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+public class FacebookController : MonoBehaviour
+{
+	#if UNITY_IPHONE || UNITY_ANDROID	
+	protected string lastResponse = "";
+
+	static FacebookController instance;
+	private string _userId;
+
+	public static FacebookController Instance	
+	{
+		get 
+		{
+			if (instance == null)
+			{
+				instance = (new GameObject("FacebookController")).AddComponent<FacebookController>();
+				instance.Initialize();
+				DontDestroyOnLoad( instance.gameObject );
+			}
+			return instance;
+		}
+	}
+
+	void Initialize()
+	{
+		CallFBInit();
+	}
+
+	#region FB.Init()
+	
+	private void CallFBInit()
+	{
+		FB.Init(OnInitComplete, OnHideUnity);
+	}
+	
+	private void OnInitComplete()
+	{
+		Debug.Log("FB.Init completed: Is user logged in? " + FB.IsLoggedIn);
+	}
+	
+	private void OnHideUnity(bool isGameShown)
+	{
+		Debug.Log("Is game showing? " + isGameShown);
+	}
+	
+	#endregion
+
+
+	#region FB.Login()
+	public void Login ()
+	{
+		CallFBLogin();
+	}
+	
+	private void CallFBLogin()
+	{
+		FB.Login("public_profile,email,user_friends", LoginCallback);
+	}
+	
+	private void CallFBLoginForPublish()
+	{
+		// It is generally good behavior to split asking for read and publish
+		// permissions rather than ask for them all at once.
+		//
+		// In your own game, consider postponing this call until the moment
+		// you actually need it.
+		FB.Login("publish_actions", LoginCallback);
+	}
+	
+	void LoginCallback(FBResult result)
+	{
+		if (result.Error != null)
+			lastResponse = "Error Response:\n" + result.Error;
+		else if (!FB.IsLoggedIn)
+		{
+			lastResponse = "Login cancelled by Player";
+		}
+		else
+		{
+			lastResponse = "Login was successful!";
+		}
+	}
+	
+	private void CallFBLogout()
+	{
+		FB.Logout();
+	}
+
+	public bool isSessionValid ()
+	{
+		return FB.IsLoggedIn;
+	}
+	#endregion
+
+
+	#region FB.Feed() example
+	
+	public string FeedToId = "";
+	public string FeedLink = "";
+	public string FeedLinkName = "";
+	public string FeedLinkCaption = "";
+	public string FeedLinkDescription = "";
+	public string FeedPicture = "";
+	public string FeedMediaSource = "";
+	public string FeedActionName = "";
+	public string FeedActionLink = "";
+	public string FeedReference = "";
+	public bool IncludeFeedProperties = false;
+	private Dictionary<string, string[]> FeedProperties = new Dictionary<string, string[]>();
+
+	public void ShowShareDialog ( string link, string name, string picture, string caption, string description )
+	{
+		FeedLink = link;
+		FeedLinkName = name;
+		FeedPicture = picture;
+		FeedLinkCaption = caption;
+		FeedLinkDescription = description;
+
+		CallFBFeed ();
+	}
+
+	private void CallFBFeed()
+	{
+		Dictionary<string, string[]> feedProperties = null;
+		if (IncludeFeedProperties)
+		{
+			feedProperties = FeedProperties;
+		}
+		FB.Feed(
+			toId: FeedToId,
+			link: FeedLink,
+			linkName: FeedLinkName,
+			linkCaption: FeedLinkCaption,
+			linkDescription: FeedLinkDescription,
+			picture: FeedPicture,
+			mediaSource: FeedMediaSource,
+			actionName: FeedActionName,
+			actionLink: FeedActionLink,
+			reference: FeedReference,
+			properties: feedProperties
+			);
+	}
+	
+	#endregion
+
+	#endif
+}
---
+++ Bubble Pop/Assets/Controllers/TwitterController.cs
@@ -0,0 +1,78 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using Prime31;
+
+public class TwitterController : MonoBehaviour
+{
+	#if UNITY_IPHONE || UNITY_ANDROID
+	static TwitterController instance;
+	public static TwitterController Instance	
+	{
+		get 
+		{
+			if (instance == null)
+			{
+				instance = (new GameObject("TwitterController")).AddComponent<TwitterController>();
+				instance.Initialize();
+				DontDestroyOnLoad( instance.gameObject );
+			}
+			return instance;
+		}
+	}
+
+	// common event handler used for all Twitter API requests that logs the data to the console
+	void completionHandler( string error, object result )
+	{
+		if( error != null )
+			Debug.LogError( error );
+		else
+			Prime31.Utils.logObject( result );
+	}
+
+	public void Initialize ()
+	{
+		TwitterCombo.init( ConfigManager.TWITTER_CONSUMER_KEY , ConfigManager.TWITTER_CONSUMER_SECRET );
+	}
+
+	public void Login ()
+	{
+		TwitterCombo.showLoginDialog();
+	}
+
+	public void Logout ()
+	{
+		TwitterCombo.logout();
+	}
+
+	public bool isLoggedIn ()
+	{
+		bool isLoggedIn = TwitterCombo.isLoggedIn();
+		return isLoggedIn;
+	}
+
+	public string LoggedInUsername ()
+	{
+		string username = TwitterCombo.loggedInUsername();
+		return username;
+	}
+
+	public void PostScore ( string score )
+	{
+		TwitterCombo.postStatusUpdate( score );
+	}
+
+	public void PostScoreWithImage ( string score, string imagePath )
+	{
+		//var pathToImage = Application.persistentDataPath + "/" + FacebookComboUI.screenshotFilename;
+		TwitterCombo.postStatusUpdate( score, imagePath);
+	}
+
+	public void CustomRequest ()
+	{
+		var dict = new Dictionary<string,string>();
+		dict.Add( "count", "2" );
+		TwitterCombo.performRequest( "GET", "1.1/statuses/home_timeline.json", dict );
+	}
+	#endif
+}
